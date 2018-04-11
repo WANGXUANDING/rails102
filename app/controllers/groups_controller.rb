ab2120daf726863b920ecd407da5a1040ee85559
WANGXUANDING <wangxuanding1@gmail.com>
2018-04-11 08:44:50 +0200

    when created a group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
@@ -16,7 +16,8 @@ class GroupsController < ApplicationController
   def create
   @group = Group.new(group_params)
   @group.user = current_user
-  if @group.save
+    if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -34,6 +35,32 @@ class GroupsController < ApplicationController
     flash[:alert] = "Group deleted"
     redirect_to groups_path
   end
+
+  def join
+    @group = Group.find(params[:id])
+
+      if !current_user.is_member_of?(@group)
+        current_user.join!(@group)
+        flash[:notice] = "successed"
+      else
+        flash[:warning] = "failed"
+      end
+      redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "quited"
+    else
+      flash[:alett] = "ur not a member "
+    end
+
+    redirect_to group_path(@group)
+  end
+
   private
   def find_group_and_check_permission
     @group = Group.find(params[:id])
